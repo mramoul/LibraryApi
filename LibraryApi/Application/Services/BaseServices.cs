af12ffe2fd6da15e3704fb187f03f832126de4fc
Binary-Zero <ramoulmohamedislem@gmail.com>
2025-03-20 18:41:33 -0400

    Add GetById service for Entity

---
+++ LibraryApi/Application/Services/BaseServices.cs
@@ -0,0 +1,14 @@
+using LibraryApi.Domain;
+using LibraryApi.Infrastructure.DataBaseContext;
+
+namespace LibraryApi.Application.Services
+{
+    public class BaseServices<TEntity>(IApplicationDbContext dbContext): IBaseServices<TEntity> where TEntity: Entity
+    {
+        Task<TEntity?> IBaseServices<TEntity>.GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var entity = dbContext.RetrieveAsync<TEntity>(id, cancellationToken);
+            return entity;
+        }
+    }
+}
