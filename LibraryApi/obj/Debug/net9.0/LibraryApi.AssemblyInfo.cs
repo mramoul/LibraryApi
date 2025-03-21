e902686829ba3a3226a99c03e8e42c59c9cebbd7
Binary-Zero <ramoulmohamedislem@gmail.com>
2025-03-21 17:34:18 -0400

    Add Delete operation to the DBContext

--- LibraryApi/obj/Debug/net9.0/LibraryApi.AssemblyInfo.cs
+++ LibraryApi/obj/Debug/net9.0/LibraryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f79df463704223a7fad9d32b204ca87f06e95d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745d4f5de5669fa7ce40f73b7295fb67f9318aba")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
