3a1e3c3d52f82ed85483adf1089de743de6cb74d
Binary-Zero <ramoulmohamedislem@gmail.com>
2025-03-22 21:45:29 -0400

    Minor clean-up

--- LibraryApi/obj/Debug/net9.0/LibraryApi.AssemblyInfo.cs
+++ LibraryApi/obj/Debug/net9.0/LibraryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47eceddd69f12c5d65987cc8391b8d212f50dc75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c6697a7de22fc50b827b79b75b490e5d0949910")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
